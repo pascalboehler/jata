f8a5295cebd14dbb3074c7242ee4dd3e40c422d9
Pascal Böhler <pascal.boehler@hotmail.de>
2022-11-03 15:43:05 +0100

    feat: Added import calls to read tokens from env file

--- data_helpers/twitter_api_handler.py
+++ data_helpers/twitter_api_handler.py
@@ -6,13 +6,13 @@
 # Copyright © 2020 Pascal Boehler. All rights reserved.
 #
 
-from tweet.py import Tweet
+from tweet import Tweet
 from dotenv import load_dotenv
 from dotenv import dotenv_values
 
 class TwitterApiHandler:
 
-    def __init__():
+    def __init__(self):
         # load api login data from .env file
         path_to_env = "./twitter_api.env" # might need to be fixed later
 
@@ -21,7 +21,19 @@ class TwitterApiHandler:
         api_key = api_conf["api_key"]
         api_secret = api_conf["api_secret"]
         bearer_token=api_conf["bearer_token"]
-        
-         
+
+        access_token=api_conf["access_token"]
+        access_token_secret = api_conf["access_token_secret"]
+
+        print(api_key)
+        print(api_secret)
+        print(bearer_token)
+        print(access_token)
+        print(access_token_secret)
+
+
 
         pass
+
+if __name__ == "__main__":
+    handler = TwitterApiHandler()
